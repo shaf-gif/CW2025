a18a5bb7da300e456f8a8f90b2e275e6eda19ecb
shaf-gif <shafoonhafiz0709@gmail.com>
2025-11-28 02:29:10 +0800

    feat:add settings in main menu with controls screen

---
+++ src/main/java/com/comp2042/ui/ControlsController.java
@@ -0,0 +1,25 @@
+package com.comp2042.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class ControlsController {
+
+    public void backToSettings(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+        URL location = getClass().getClassLoader().getResource("settingsLayout.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(location);
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root, 600, 510);
+        stage.setScene(scene);
+        stage.show();
+    }
+} 
